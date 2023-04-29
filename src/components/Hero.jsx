2dc5bc5bb2c2f4c95151d6e37de1e466bf0af32d
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-04-29 11:35:41 +0300

    Added hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import heropic from "../images/heropic.jpg";
-import {ImLocation} from "react-icons/im";
+import { ImLocation } from "react-icons/im";
+import onek from "../images/1k.png";
+import house2 from "../images/house2.jpeg";
 const Hero = () => {
   return (
     <div className="flex justify-between items-center">
@@ -43,14 +45,45 @@ const Hero = () => {
       </div>
 
       <div
-        className="bg-cover bg-center bg-no-repeat"
+        className="bg-cover flex flex-col  p-4 justify-between bg-center bg-no-repeat"
         style={{
           backgroundImage: `url(${heropic})`,
           width: "720px",
           height: "720px",
           borderRadius: "0px 0px 0px 80px",
         }}
-      ></div>
+      >
+        <div className="flex w-[80%] mx-auto text-white  font-bold justify-around">
+          <h1 className="border-white border-2 cursor-pointer hover:scale-105 transition-all duration-300  rounded-3xl p-2">
+            Featured
+          </h1>
+          <h1 className="border-white border-2  cursor-pointer hover:scale-105 transition-all duration-300 rounded-3xl p-2">
+            Reviews
+          </h1>
+          <h1 className="border-white border-2  cursor-pointer hover:scale-105 transition-all duration-300 rounded-3xl p-2">
+            Articles
+          </h1>
+          <h1 className="border-white border-2 cursor-pointer hover:scale-105 transition-all duration-300  rounded-3xl p-2">
+            Ready to sell
+          </h1>
+        </div>
+        <div className="flex justify-around">
+          <div className="bg-white gap-2 flex justify-between rounded-3xl p-4">
+            <img src={onek} alt="" className="w-20 " />
+            <div className="flex flex-col gap-1 justify-between">
+              <h1 className="text-xl font-bold text-[1B1C57]">1k + People</h1>
+              <p className="text-sm text-[68799F]">Successfully Getting Home</p>
+            </div>
+          </div>
+          <div className="bg-white gap-2 flex justify-between rounded-3xl p-4">
+            <img src={house2} alt="" className="w-10 h-10 rounded-full" />
+            <div className="flex flex-col  justify-between">
+              <h1 className="text-xl font-bold text-[1B1C57]">56 houses</h1>
+              <p className="text-sm text-[68799F]">Sold Monthly</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
